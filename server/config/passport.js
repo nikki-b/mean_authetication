8de6e6eed1706c3b6261282f03e48bf99d708732
nikki-b <nikki.m.brown@gmail.com>
2015-09-29 12:33:35 -0700

    facebook added

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,6 +1,8 @@
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 var mongoose = require('mongoose');
 var User = mongoose.model("User");
+var configAuth = require('./auth');
 
 // expose this function to our app using module.exports
 module.exports = function(passport) {
@@ -58,28 +60,72 @@ module.exports = function(passport) {
 
   // LOCAL - LOGIN
   passport.use('local-login', new LocalStrategy({
-    // by default, local strategy uses username and password,
-    // we will override with email
-    usernameField : 'email',
-    passwordField : 'password',
-    passReqToCallback : true
-  },
-  function(req, email, password, done) {
-    // find a user whose email is the same as the forms email
-    // we are checking to see if the user trying to login already exists
-    User.findOne({ 'local.email' :  email }, function(err, user) {
-      // if there are any errors, return the error before anything else
-      if (err)
-          return done(err);
-      // if no user is found, return the message
-      if (!user)
-          return done(null, false, req.flash('loginMessage', 'Invalid credentials.'));
-      // if the user is found but the password is wrong
-      if (!user.validPassword(password))
-          return done(null, false, req.flash('loginMessage', 'Invalid credentials.'));
-      // all is well, return successful user
-      return done(null, user);
-    }); // end of User.findOne
-  } // end of anon function
+      // by default, local strategy uses username and password,
+      // we will override with email
+      usernameField : 'email',
+      passwordField : 'password',
+      passReqToCallback : true
+    },
+    function(req, email, password, done) {
+      // find a user whose email is the same as the forms email
+      // we are checking to see if the user trying to login already exists
+      User.findOne({ 'local.email' :  email }, function(err, user) {
+        // if there are any errors, return the error before anything else
+        if (err)
+            return done(err);
+        // if no user is found, return the message
+        if (!user)
+            return done(null, false, req.flash('loginMessage', 'Invalid credentials.'));
+        // if the user is found but the password is wrong
+        if (!user.validPassword(password))
+            return done(null, false, req.flash('loginMessage', 'Invalid credentials.'));
+        // all is well, return successful user
+        return done(null, user);
+      }); // end of User.findOne
+    } // end of anon function
   )) // end of local-login
+
+  // FACEBOOK - LOGIN
+  passport.use(new FacebookStrategy({
+      // pull in our app id and secret from our auth.js file
+      clientID        : configAuth.facebookAuth.clientID,
+      clientSecret    : configAuth.facebookAuth.clientSecret,
+      callbackURL     : configAuth.facebookAuth.callbackURL,
+      profileFields   : ["emails", "displayName"]
+    },
+    // facebook will send back the token and profile
+    function(token, refreshToken, profile, done) {
+      // asynchronous
+      process.nextTick(function() {
+        // find the user in the database based on their facebook id
+        User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+          // if there is an error, stop everything and return that
+          // ie an error connecting to the database
+          if (err)
+              return done(err);
+
+          // if the user is found, then log them in
+          if (user) {
+              return done(null, user); // user found, return that user
+          } 
+          else {
+            // if there is no user found with that facebook id, create them
+            var newUser = new User();
+            // set all of the facebook information in our user model
+            newUser.facebook.id    = profile.id;                
+            newUser.facebook.token = token;                   
+            newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+            newUser.facebook.email = profile.emails[0].value;
+            // save our user to the database
+            newUser.save(function(err) {
+              if (err)
+                throw err;
+              // if successful, return the new user
+              return done(null, newUser);
+            }); // end of save function
+          } // end of else
+        }); // end of User.findOne
+      }); // end of process.nextTick
+    } // end of anon function
+  )); // end of facebook strategy
 }; // end of module object
--- server/config/routes.js
+++ server/config/routes.js
@@ -44,6 +44,17 @@ module.exports = function(app, passport) {
         req.logout();
         res.redirect('/');
     });
+
+    // FACEBOOK
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        })
+    );
+
 };
 
 // route middleware to make sure a user is logged in
